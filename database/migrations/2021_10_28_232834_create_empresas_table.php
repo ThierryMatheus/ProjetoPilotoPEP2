2c7d6992af728fc3644794396b43a503eb8f5989
Huanderson Lima <mattewlima16@gmail.com>
2021-10-28 23:21:33 -0300

    GH-12 - Migration de criação da tabela Empresa

---
+++ database/migrations/2021_10_28_232834_create_empresas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('nome_fantasia');
+            $table->integer('tipo');
+            $table->integer('cnpj');
+            $table->string('email')->unique();
+            $table->string('senha');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}

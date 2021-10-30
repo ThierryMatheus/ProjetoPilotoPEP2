67beb5afa2db8d54d65dfa8f601a0c7bbe78199e
Gleydson José <gleydson_jose333@outlook.com>
2021-10-30 00:23:51 -0300

    GH-11 - Alteração na migrations do usuário

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('nome');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('senha');
+            $table->string('password');
             $table->string('genero');
             $table->bigInteger('cpf');
             $table->date('data_nascimento');
